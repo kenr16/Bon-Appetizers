5b5dc8d6deaab376a16603131285b57e02d49494
Ken Rutan <kenr16@gmail.com>
2017-07-07 12:52:18 -0700

    add unit spec testing

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,4 +5,8 @@ describe Product do
   it { should validate_presence_of :description }
   it { should validate_presence_of :category }
   it { should validate_presence_of :price }
+  it { should have_many :reviews }
+  it { validate_length_of :name }
+  it { validate_length_of :description }
+
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,4 +4,10 @@ describe Review do
   it { should validate_presence_of :author }
   it { should validate_presence_of :content }
   it { should validate_presence_of :rating }
+  it { should belong_to :product }
+
+  it { should have_db_column :product_id }
+  it { should validate_numericality_of :rating }
+  it { should validate_length_of :content }
+
 end
